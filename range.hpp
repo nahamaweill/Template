fa3893cd034632a249ca2f947edbadc11fe20269
nahamaweill <nahamaweill@gmail.com>
2019-05-12 17:05:30 +0300

    range ok

--- range.hpp
+++ range.hpp
@@ -1,11 +1,59 @@
+#pragma once
+namespace itertools{
+    template <typename T>
+    class range
+    {
+        private:
+        T start;
+        T end;
 
-template <typename T>
+        public:
 
-class range
-{
-private:
-    /* data */
-public:
-    range(T start, T end);
-    ~range();
-};
+        range(T _start, T _end) : start(_start), end(_end) {
+
+        }
+
+        ~range(){
+
+        }
+
+        class iterator
+        {
+            private:
+            T data;
+
+            public:
+            iterator(T ptr = NULL) : data(ptr) {
+
+            }
+
+            T& operator*() const {
+			    return data;
+            }
+
+            iterator& operator++() {
+                ++data
+			    return *this;
+            }
+
+		    bool operator==(const iterator& rhs) const {
+			    return (data == rhs->data);
+		    }
+
+		    bool operator!=(const iterator& rhs) const {
+			    return (data != rhs->data);
+            }
+        };
+
+        public:
+
+        iterator begin() {
+		    return iterator(start);
+	    }
+	
+	    iterator end() {
+		    return iterator(end);
+        }
+
+    };
+}
