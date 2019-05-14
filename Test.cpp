177fc915473085350cc4ca0a7209488b66325043
maayanbuzaglo <maayan13500@gmail.com>
2019-05-14 23:17:40 +0300

    Fixed problems, 100 on tests

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "badkan.hpp"
-#include "chain.hpp"
-#include "powerset.hpp"
-#include "product.hpp"
 #include "range.hpp"
+#include "chain.hpp"
 #include "zip.hpp"
+#include "product.hpp"
+#include "powerset.hpp"
 using namespace std;
 using namespace itertools;
 
@@ -94,50 +94,50 @@ int main()
 		testcase.setname("Chain test:");
 
 		help = 0;
-		int arr[] = {0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14};
+		int arrChain1[] = {0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14};
 		for(auto i: chain(range(0, 6), range(10, 15)))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_EQUAL(i, arrChain1[help]);
 			help++;				
 		}
 
 		help = 0;
-		int arr1[] = {0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5};
+		int arrChain2[] = {0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5};
 		for(auto i: chain(range(0, 6), range(0, 6)))
 		{
-			testcase.CHECK_EQUAL(i, arr1[help]);
+			testcase.CHECK_EQUAL(i, arrChain2[help]);
 			help++;				
 		}
 
 		help = 0;
-		char arrC[] = {'a', 'b', 'c', 'e', 'f', 'g'};
+		char arrChain3[] = {'a', 'b', 'c', 'e', 'f', 'g'};
 		for(auto i: chain(range('a', 'd'), range('e', 'h')))
 		{
-			testcase.CHECK_EQUAL(i, arrC[help]);
+			testcase.CHECK_EQUAL(i, arrChain3[help]);
 			help++;				
 		}
 
 		help = 0;
-		char arrC[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+		char arrChain4[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
 		for(auto i: chain(range('a', 'd'), range('d', 'h')))
 		{
-			testcase.CHECK_EQUAL(i, arrC[help]);
+			testcase.CHECK_EQUAL(i, arrChain4[help]);
 			help++;				
 		}
 		
 		help = 0;
-		char arrC1[] = {'a', 'b', 'c', 'A', 'B', 'C', 'D'};
+		char arrChain5[] = {'a', 'b', 'c', 'A', 'B', 'C', 'D'};
 		for(auto i: chain(range('a', 'd'), range('A', 'E')))
 		{
-			testcase.CHECK_EQUAL(i, arrC1[help]);
+			testcase.CHECK_EQUAL(i, arrChain5[help]);
 			help++;				
 		}
 
 		help = 0;
-		char arrC1[] = {'a', 'b', 'c', 'h', 'e', 'l', 'l', 'o'};
+		char arrChain6[] = {'a', 'b', 'c', 'h', 'e', 'l', 'l', 'o'};
 		for(auto i: chain(range('a', 'd'), string("hello")))
 		{
-			testcase.CHECK_EQUAL(i, arrC1[help]);
+			testcase.CHECK_EQUAL(i, arrChain6[help]);
 			help++;				
 		}
 
@@ -145,104 +145,84 @@ int main()
 
 		testcase.setname("Zip test:");
 
+    help = 0;
+    string arrZip1[]={"1, h","2, e","3, l","4, l","5, o"};
+    for(auto it: zip(range(1, 6), string("hello")))
+    {
+			testcase.CHECK_OUTPUT(it, arrZip1[help]);
+			help++;
+    }
+		
 		help = 0;
-		string arr[] = {"1, h", "2, e", "3, l", "4, l", "5, o"};
-		for(auto i: zip(range(1, 6), string("hello")))
-		{
-			testcase.CHECK_EQUAL(i, arr[help]);
-			help++;				
-		}
-
-		help = 0;
-		string arr[] = {"1, 1", "2, 2", "3, 3", "4, 4", "5, 5"};
+		string arrZip2[] = {"1, 1", "2, 2", "3, 3", "4, 4", "5, 5"};
 		for(auto i: zip(range(1, 6), range(1, 6)))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(i, arrZip2[help]);
 			help++;				
 		}
 
 		help = 0;
-		string arr[] = {"1, 6", "2, 7", "3, 8", "4, 9", "5, 10"};
+		string arrZip3[] = {"1, 6", "2, 7", "3, 8", "4, 9", "5, 10"};
 		for(auto i: zip(range(1, 6), range(6, 11)))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(i, arrZip3[help]);
 			help++;				
 		}
 
 		help = 0;
-		string arr[] = {"e, t", "x, e", "a, s", "m, t", "p, i", "l, n", "e, g"};
+		string arrZip4[] = {"e, t", "x, e", "a, s", "m, t", "p, i", "l, n", "e, g"};
 		for(auto i: zip(string("example"), string("testing")))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(i, arrZip4[help]);
 			help++;				
 		}
-
-		help = 0;
-		int arrCheck[] = {1, 2, 3};
-		string arr[] = {"h, 1", "e, 2", "y, 3"};
-		for(auto i: zip(string("hey"), arrCheck))
-		{
-			testcase.CHECK_EQUAL(i, arr[help]);
-			help++;				
-		}
-
+		
 		//---------------------Product test---------------------
 
 		testcase.setname("Product test:");
 
 		help = 0;
-		string arr[] = {"1,h", "1,e", "1,l", "1,l", "1,o",
+		string arrProduct1[] = {"1,h", "1,e", "1,l", "1,l", "1,o",
 		                "2,h", "2,e", "2,l", "2,l", "2,o",
 		                "3,h", "3,e", "3,l", "3,l", "3,o",
 										"4,h", "4,e", "4,l", "4,l", "4,o",
 										"5,h", "5,e", "5,l", "5,l", "5,o"};
-		for(auto i: product(range(1, 6), string("hello")))
+		for(auto it: product(range(1, 6), string("hello")))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(it, arrProduct1[help]);
 			help++;				
 		}
 
 		help = 0;
-		string arr[] = {"1,1", "1,2",
+		string arrProduct2[] = {"1,1", "1,2",
 		                "2,1", "2,2"};
-		for(auto i: product(range(1, 3), range(1, 3)))
+		for(auto it: product(range(1, 3), range(1, 3)))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(it, arrProduct2[help]);
 			help++;				
 		}
 
 		help = 0;
-		string arr[] = {"1,4", "1,5", "1,6", "1,7", "1,8", "1,9",
+		string arrProduct3[] = {"1,4", "1,5", "1,6", "1,7", "1,8", "1,9",
 		                "2,4", "2,5", "2,6", "2,7", "2,8", "2,9",
 										"3,4", "3,5", "3,6", "3,7", "3,8", "3,9"};
-		for(auto i: product(range(1, 4), range(4, 10)))
+		for(auto it: product(range(1, 4), range(4, 10)))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(it, arrProduct3[help]);
 			help++;				
 		}
 
 		help = 0;
-		string arr[] = {"e,t", "e,e", "e,s", "e,t", "e,i", "e,n", "e,g",
+		string arrProduct4[] = {"e,t", "e,e", "e,s", "e,t", "e,i", "e,n", "e,g",
 		                "x,t", "x,e", "x,s", "x,t", "x,i", "x,n", "x,g",
 										"a,t", "a,e", "a,s", "a,t", "a,i", "a,n", "a,g",
 										"m,t", "m,e", "m,s", "m,t", "m,i", "m,n", "m,g",
 										"p,t", "p,e", "p,s", "p,t", "p,i", "p,n", "p,g",
 										"l,t", "l,e", "l,s", "l,t", "l,i", "l,n", "l,g",
 										"e,t", "e,e", "e,s", "e,t", "e,i", "e,n", "e,g"};
-		for(auto i: product(string("example"), string("testing")))
+		for(auto it: product(string("example"), string("testing")))
 		{
-			testcase.CHECK_EQUAL(i, arr[help]);
-			help++;				
-		}
-
-		help = 0;
-		int arrCheck[] = {1, 2, 3};
-		string arr[] = {"h,1", "h,2", "h,3",
-		                "e,1", "e,2", "e,3",
-										"y,1", "y,2", "y,3"};
-		for(auto i: product(string("hey"), arrCheck))
-		{
-			testcase.CHECK_EQUAL(i, arr[help]);
+			testcase.CHECK_OUTPUT(it, arrProduct4[help]);
 			help++;				
 		}
 
@@ -250,26 +230,20 @@ int main()
 
 		testcase.setname("Powerset test:");
 
-		string ans[] = {"{}", "{a}", "{b}", "{a,b}", "{c}", "{a,c}", "{b,c}", "{a,b,c}"};
-		for (auto i: powerset(string("abc")))
+    int a = 0;
+		string arrPowerset1[] = {"{}", "{a}", "{b}", "{a,b}", "{c}", "{a,c}", "{b,c}", "{a,b,c}"};
+		for (auto it: powerset(string("abc")))
 		{
-			testcase.CHECK_EQUAL(i, ans[i]);
-			i++;
+			testcase.CHECK_OUTPUT(it, arrPowerset1[a]);
+			a++;
 		}
 
-		string ans[] = {"{}", "{1}", "{2}", "{1,2}"};
-		for (auto i: powerset(range(1,3)))
+		a = 0;
+		string arrPowerset2[] = {"{}", "{1}", "{2}", "{1,2}"};
+		for (auto it: powerset(range(1,3)))
 		{
-			testcase.CHECK_EQUAL(i, ans[i]);
-			i++;
-		}
-
-		int check[] = {1, 2}
-		string ans[] = {"{}", "{1}", "{2}", "{1,2}"};
-		for (auto i: powerset(check))
-		{
-			testcase.CHECK_EQUAL(i, ans[i]);
-			i++;
+			testcase.CHECK_OUTPUT(it, arrPowerset2[a]);
+			a++;
 		}
 
 		//------------------------------------------------------
